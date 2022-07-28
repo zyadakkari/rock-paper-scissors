10b867cb3047063daa0e5232dcfa13bdffad4ebd
Zyad Akkari <akkarizyad@gmail.com>
2022-07-28 13:06:41 +0100

    Updated with code comments

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-// tell the user we are playing RPS and they need to select one
+// tell the user we are playing Rock paper scissors
 console.log(alert("Hi! Let's play rock, paper scissors. To play just type your selection in the next window"))
-// prompt user to give their selection
+// variables for keeping score
 let userTotal = 0
 let computerTotal = 0
+// overall game function
 function playGame (){
+    // gets user choice
     let getUser = function (getUser){
         return prompt("rock, paper, scissors")
     }
     let userChoice = getUser()
-
+    // gets computer choice
     let getComputer = function (getComputer){
         let options = ["rock", "paper", "scissors"]
         return options[Math.floor(Math.random() * options.length)];
     }
     let computerChoice = getComputer()
     console.log(`You chose ${userChoice}, I choose ${computerChoice}`)
+    // evaluates who won
     let decideWinner = function (win){
         let userScore = userChoice.length;
         let computerScore = computerChoice.length;
@@ -31,6 +34,7 @@ function playGame (){
     }
 
     let winner = decideWinner()
+    // tells user who won and updates the aggregate score
     if (winner === "user") {
         console.log("You won! Congrats!")
         userTotal += 1
@@ -41,6 +45,7 @@ function playGame (){
     }
     else {console.log("It's a draw! Boring!")}
     console.log(`The score is You: ${userTotal} Computer: ${computerTotal}`)
+    // tells user how to play again
     console.log("To play again type 'playGame()' in the console")
 
 
